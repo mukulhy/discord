68a180cb8db43ff434a1c4628608c2affce13232
mukulhy <yadavh3034@gmail.com>
2023-12-31 17:55:32 +0530

    added curent-profile and modal component

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -2,6 +2,7 @@ import { redirect } from "next/navigation";
 
 import { db } from "@/lib/db";
 import { initialProfile } from "@/lib/initial-profile";
+import InitialModal from "@/components/modals/initial-modal";
 
 const SetupPage = async () => {
   const profile = await initialProfile();
@@ -22,7 +23,7 @@ const SetupPage = async () => {
 
   return (
     <div>
-      Create server
+      <InitialModal />
     </div>
   );
 }
---
+++ components/modals/initial-modal.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const InitialModal = () => {
+  return (
+    <div>initial-modal</div>
+  )
+}
+
+export default InitialModal     
