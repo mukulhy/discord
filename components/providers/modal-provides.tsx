41be0c5b13991017367a972a5c8d81c5f32e8713
mukulhy <yadavh3034@gmail.com>
2024-01-26 18:16:54 +0530

    add invite user

--- components/providers/modal-provides.tsx
+++ components/providers/modal-provides.tsx
@@ -1,10 +1,12 @@
 "use client";
 import CreateServerModal from "../modals/create-server-modal";
+import { InviteModal } from "../modals/invite-modal";
 
 export const ModalProviders = () => {
   return (
     <>
       <CreateServerModal />
+      <InviteModal />
     </>
   );
 };
