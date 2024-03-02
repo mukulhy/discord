1a8bfeb322eb5cf310ba055491c5d681f14feb64
mukulhy <yadavh3034@gmail.com>
2024-03-02 15:41:16 +0530

    commit channel and chat header

--- components/providers/modal-provides.tsx
+++ components/providers/modal-provides.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { CreateChannelModal } from "../modals/create-channel-modal";
 import CreateServerModal from "../modals/create-server-modal";
 import { DeleteServerModal } from "../modals/delete-server-modal";
 import { EditServerModal } from "../modals/edit-server-modal";
@@ -13,6 +14,7 @@ export const ModalProviders = () => {
       <EditServerModal />
       <DeleteServerModal />
       <InviteModal />
+      <CreateChannelModal />
       <MembersModal />
     </>
   );
